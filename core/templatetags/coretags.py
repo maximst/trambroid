e4f39a75673089cf07cdf203a62b99528af21cd9
Maxim Starodubcev <maxim.starodubtsev@chisw.us>
2012-10-05 13:00:58 +0300

    Breadcrumps placed in a separate templatetag

--- core/templatetags/coretags.py
+++ core/templatetags/coretags.py
@@ -16,3 +16,16 @@ def menubar(context):
 
     current_section = context['request'].META['PATH_INFO'].split('/')[1]
     return {'current_section': current_section, 'sections': sections}
+
+@register.inclusion_tag('breadcrump.html', takes_context=True)
+def breadcrump(context):
+    crumps = context['request'].META['PATH_INFO'].split('/')[:-1]
+
+    urls = map(lambda c: crumps[:crumps.index(c)+1], crumps)
+    urls[0].append(u'')
+
+    titles = ['Home'] + crumps[1:]
+
+    crumps = zip(urls, titles)
+    return {'crumps': crumps}
+
