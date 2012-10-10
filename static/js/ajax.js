0b82e9c6fab176c39f36c33c45a274075c581d73
Maxim Starodubcev <maxim.starodubtsev@chisw.us>
2012-10-10 18:06:20 +0300

    UI fix

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -15,11 +15,18 @@ function vote() {
             score_votes.removeClass();
         }
         vote_block.attr("title", "Score: " + data["score"] + " / Votes: " + data["num_votes"]);
-        $("#success_alert").show("slow", function() { setTimeout('$("#success_alert").hide("slow")', 4000) });
+        $("#success_alert").show(300, function() { setTimeout('$("#success_alert").hide(300)', 5000) });
     });
 }
 
+function hide_div(event) {
+    if ($(event.target).closest("#success_alert").length) return;
+    $("#success_alert").hide(300);
+    event.stopPropagation();
+}
+
 $(function() {
+    $(document).click(hide_div);
     $("#down_vote").click(vote);
     $("#up_vote").click(vote);
 });
