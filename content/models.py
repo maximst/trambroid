0c368a71b19a659444f8f32736128b192f5a5f9c
Maxim Starodubcev <maxim@trambroid.com>
2013-04-09 15:35:03 +0300

    Comments

--- content/models.py
+++ content/models.py
@@ -42,3 +42,6 @@ class Comment(models.Model):
     def __unicode__(self):
         return u'%s: %s - %s' % (self.user, self.blog, self.body[:32])
 
+
+Blog.num_comments = property(lambda b: Comment.objects.filter(blog=b).count())
+
--- content/views.py
+++ content/views.py
@@ -1,21 +1,35 @@
 # -*-coding: utf8-*-
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.context_processors import csrf
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from models import Blog
+
+from models import Blog, Comment
+from forms import CommentForm
 
 def blog_detail(request, slug):
-    print request.META['PATH_INFO']
+    user = request.user
+
     content = get_object_or_404(Blog, slug=slug)
-    contents = {'content': content}
+    comments = Comment.objects.filter(blog=content).order_by('-create_time')
+    contents = {'content': content, 'comments': comments}
     contents.update(csrf(request))
+
+    if request.method == 'POST' and user.is_authenticated():
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            ip = request.META['REMOTE_ADDR']
+            comment_form.save(content, user, ip)
+            return redirect(request.META['HTTP_REFERER'])
+    else:
+        comment_form = CommentForm()
+    contents['comment_form'] = comment_form
+
     return render(request, 'blog/blog_detail.html', contents)
 
 
 def blog_list(request):
     contents = Blog.objects.all().order_by('-create_time')
     paginator = Paginator(contents, 30)
-    print request.META['PATH_INFO']
     page = request.GET.get('p')
     try:
         content = paginator.page(page)
