ba290ae8d5045110059960ca32c8bed23ccb323a
Maxim Starodubcev <maxim@trambroid.com>
2014-10-30 15:39:07 +0200

    Change django version

--- trambroid/settings.py
+++ trambroid/settings.py
@@ -2,40 +2,68 @@
 Django settings for trambroid project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
+https://docs.djangoproject.com/en/1.6/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
+https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gn&7$cou@8p_k=5y676+8g(8@&eie2q-1)7)cu1fmghw0m_g%t'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
 
 # Application definition
 
 INSTALLED_APPS = (
+    'mezzanine.boot',
+    'mezzanine.conf',
+    'mezzanine.core',
+    'mezzanine.generic',
+    'mezzanine.pages',
+    'django.contrib.comments',
+    'django.contrib.sites',
+    'filebrowser_safe',
+    'grappelli_safe',
+    'widgy',
+    'widgy.contrib.page_builder',
+    'widgy.contrib.form_builder',
+    'widgy.contrib.widgy_mezzanine',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'filer',
+    'easy_thumbnails',
+    'compressor',
+    'argonauts',
+    'scss',
+    'sorl.thumbnail',
+    'south',
+
+    'apps.core',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -43,9 +71,11 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mezzanine.core.request.CurrentRequestMiddleware',
+    'mezzanine.core.middleware.AdminLoginInterfaceSelectorMiddleware',
+    'mezzanine.pages.middleware.PageMiddleware',
 )
 
 ROOT_URLCONF = 'trambroid.urls'
@@ -54,17 +84,17 @@ WSGI_APPLICATION = 'trambroid.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
     }
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -78,6 +108,56 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+PACKAGE_NAME_FILEBROWSER = "filebrowser_safe"
+PACKAGE_NAME_GRAPPELLI = "grappelli_safe"
+ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
+TESTING = False
+GRAPPELLI_INSTALLED = True
+SITE_ID = 1
+
+WIDGY_MEZZANINE_SITE = 'apps.core.widgy_site.site'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.static",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    "mezzanine.conf.context_processors.settings",
+    "mezzanine.pages.context_processors.page",
+)
+
+SEARCH_MODEL_CHOICES = None
+ADD_PAGE_ORDER = (
+    'widgy_mezzanine.WidgyPage',
+)
+
+STATICFILES_FINDERS = (
+    'compressor.finders.CompressorFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+COMPRESS_ENABLED = True
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_pyscss.compressor.DjangoScssFilter'),
+)
+
+SOUTH_MIGRATION_MODULES = {
+    'easy_thumbnails': 'easy_thumbnails.south_migrations',
+
+}
+
+try:
+    from settings_local import *
+except Exception as e:
+    print 'ERROR: settings_local load error -', e.message
--- trambroid/urls.py
+++ trambroid/urls.py
@@ -1,10 +1,22 @@
 from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
+admin.autodiscover()
+
+from apps.core.widgy_site import site as widgy_site
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'trambroid.views.home', name='home'),
+    # url(r'^$', 'apps.core.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'mezzanine.pages.views.page', {'slug': '/'}, name='home'),
+
     url(r'^admin/', include(admin.site.urls)),
+
+    # widgy admin
+    url(r'^admin/widgy/', include(widgy_site.urls)),
+    # widgy frontend
+    url(r'^widgy/', include('widgy.contrib.widgy_mezzanine.urls')),
+    url(r'^', include('mezzanine.urls')),
 )
