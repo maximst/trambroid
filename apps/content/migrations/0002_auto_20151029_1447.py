0c6987615b8a32996ac77d15cf3962ad111bd5ac
Maxim Starodubcev <maxim@trambroid.com>
2015-10-29 15:06:45 +0200

    Change migrations

---
+++ apps/content/migrations/0002_auto_20151029_1447.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import taggit.managers
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('taggit', '0002_auto_20150616_2121'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('content', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='comment',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True),
+        ),
+        migrations.AddField(
+            model_name='blogtranslation',
+            name='master',
+            field=models.ForeignKey(related_name='translations', editable=False, to='content.Blog', null=True),
+        ),
+        migrations.AddField(
+            model_name='blog',
+            name='tags',
+            field=taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', help_text='A comma-separated list of tags.', verbose_name='Tags'),
+        ),
+        migrations.AddField(
+            model_name='blog',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True),
+        ),
+        migrations.AlterUniqueTogether(
+            name='blogtranslation',
+            unique_together=set([('language_code', 'master')]),
+        ),
+    ]
