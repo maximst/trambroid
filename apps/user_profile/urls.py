0490bf12552f3bb6081fc728fbfd5457b3deae0c
Maxim Starodubcev <maxim@trambroid.com>
2015-10-29 16:08:21 +0200

    Fix auth with cyrilic password

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import profile
 
 urlpatterns = [
-    url(r'^(profile|user|users)/(?:(?P<username>\w+)/)?$', profile, name='profile', ),
+    url(r'^(?:accounts/)?(profile|user|users)/(?:(?P<username>\w+)/)?$', profile, name='profile', ),
     url(r'^accounts/$', profile),
 ]
