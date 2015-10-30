7c2dc23ea7ec6f772b7bf7b172b328d32efd58ea
Maxim Starodubcev <maxim@trambroid.com>
2015-10-30 19:25:36 +0200

    Add yandex metric

--- apps/content/urls.py
+++ apps/content/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     url(r'^blog/$', blog_list, name='list'),
 
     # Drupal URLs
-    url(r'^(?:(?P<lang>ru|en)/)?(?P<slug>content/.+)/$', blog_detail, {'is_drupal': True}),
+    url(r'^(?:(?P<lang>ru|en)/)?(?P<slug>content\/[^/]+)/$', blog_detail, {'is_drupal': True}),
     url(r'^(?:(?P<lang>ru|en)/)?node/(?P<slug>\d+)/$', blog_detail, {'is_drupal': True}),
-    url(r'^(?:(?P<lang>ru|en)/)?(?:poll|news_nix|content)/', blog_list),
+    url(r'^(?:(?P<lang>ru|en)/)?(?:poll|news_nix|content)/$', blog_list),
     url(r'^(?:(?P<lang>ru|en)/(?:node/)?|node/)$', blog_list),
     url(r'^(?:(?P<lang>ru|en)/)?(?P<drupal_blogs_alias_url>blogs/.+)/$', blog_list),
 #    url(r'^(?:(?P<lang>ru|en)/)?blog/(?P<drupal_uid>\d+)/$', blog_list),
